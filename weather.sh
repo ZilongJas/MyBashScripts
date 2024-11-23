ea4836b678695c2f913f8259cf318f916a4e9ea3
Jason <165133185+ZilongJas@users.noreply.github.com>
2024-11-23 06:06:05 -0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -2,21 +2,20 @@
 #using https://www.weatherapi.com/docs/
 #free 1m api calls per month
 
-line="==========================="
-
+# inputs:
 echo -n 'Please enter the Weather API key: '
 read -sr WEATHER_API_KEY
 echo -ne '\nPlease enter the location: '
 read -r LOCATION
 clear
-
 LOCATION=${LOCATION// /%20}
+
 response="$(curl -s "https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${LOCATION}")"
 temperature="$(echo "${response}" | jq -r '.current.temp_f')"
 humidity="$(echo "${response}" | jq -r '.current.humidity')"
 
-echo $line
+# output:
+echo "~~~ 🌤️ WEATHER  ~~~"
 echo "Temperature: ${temperature}°F"
 echo "Humidity: ${humidity}%"
-echo $line
-
+echo "~~~~~~~~~~~~~~~~~~~"
